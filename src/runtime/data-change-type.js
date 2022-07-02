66de24861542b1052497cc18167f987706fc42a8
无为 <3435516833@qq.com>
2022-07-03 02:12:44 +0800

    完成set方法

---
+++ src/runtime/data-change-type.js
@@ -0,0 +1,11 @@
+/**
+ * 数据变更类型枚举
+ *
+ * @const
+ * @type {Object}
+ */
+
+export let DataChangeType = {
+    SET: 1,
+    SPLICE: 2
+}
--- src/runtime/data.js
+++ src/runtime/data.js
@@ -1,5 +1,8 @@
+import { ExprType } from '../parser/expr-type.js'
 import { parseExpr } from '../parser/parse-Expr.js'
 import { evalExpr } from './eval-expr.js'
+import { DataChangeType } from './data-change-type.js'
+
 //响应式数据方法
 //parent为父级数据
 //data为初始数据
@@ -8,6 +11,51 @@ function Data (data, parent) {
     this.parent = parent
     this.raw = data || {}
     this.listeners = []
+}
+
+/**
+ * DataTypes 检测
+ */
+ Data.prototype.checkDataTypes = function () {
+    if (this.typeChecker) {
+        this.typeChecker(this.raw);
+    }
+};
+
+/**
+ * 设置 type checker
+ *
+ * @param  {Function} typeChecker 类型校验器
+ */
+Data.prototype.setTypeChecker = function (typeChecker) {
+    this.typeChecker = typeChecker;
+};
+
+/**
+ * 添加数据变更的事件监听器
+ *
+ * @param {Function} listener 监听函数
+ */
+Date.prototype.listen = function (listener) {
+
+}
+
+/**
+ * 移除数据变更的事件监听器
+ *
+ * @param {Function} listener 监听函数
+ */
+Date.prototype.unlisten = function (listener) {
+
+}
+
+/**
+ * 触发数据变更
+ *
+ * @param {Object} change 变更信息对象
+ */
+Data.prototype.fire = function (change) {
+
 }
 
 //该方法为获取数据
@@ -57,8 +105,52 @@ Data.prototype.get = function (expr, callee) {
  * @param {Data} data 对应的Data对象
  * @return {*} 变更后的新数据
  */
-Data.prototype.immutableSet = function (source, exprPaths, pathsStart, pathsLen, value, data) {
+function immutableSet (source, exprPaths, pathsStart, pathsLen, value, data) {
+    if(pathsStart >= pathsLen){
+        return value
+    }
 
+    if(source == null){
+        source = {}
+    }
+
+    let pathExpr = exprPaths[pathsStart]
+    let prop = evalExpr(pathExpr, data)
+    let result = source
+
+    if(source instanceof Array){
+        let index = +prop
+        prop = isNaN(index) ? prop : index
+        result = source.slice(0)
+        result[prop] = immutableSet(source[prop], exprPaths, pathsStart + 1, pathsLen, value, data)
+    }else if(typeof source === 'object'){
+        result = {}
+        let needAssigned = true
+
+        for(let key in source){
+            if(source.hasOwnProperty(key)){
+                if(key === prop){
+                    needAssigned = false
+                    result[prop] = immutableSet(source[prop], exprPaths, pathsStart + 1, pathsLen, value, data)
+                }else {
+                    result[key] = result[key]
+                }
+            }
+        }
+
+        if(needAssigned){
+            result[prop] = immutableSet(source[prop], exprPaths, pathsStart + 1, pathsLen, value, data)
+        }
+    }
+
+    if(pathExpr.value == null){
+        exprPaths[pathsStart] = {
+            type: typeof prop === 'string' ? ExprType.STRING : ExprType.NUMBER,
+            value: prop
+        }
+    }
+
+    return result
 }
 
 //改变数据
@@ -71,7 +163,36 @@ Data.prototype.immutableSet = function (source, exprPaths, pathsStart, pathsLen,
  * @param {boolean} option.silent 静默设置，不触发变更事件
  */
 Data.prototype.set = function (expr, value, option) {
+    option = option || {}
 
+    let exprRaw = expr
+
+    expr = parseExpr(expr)
+
+    if(expr.type !== ExprType.ACCESSOR){
+        throw new Error('[SAN ERROR] Invalid Expression in Data set: ' + exprRaw)
+    }
+
+    if(this.get(expr) === value && !option.force){
+        return
+    }
+
+    expr = {
+        type: ExprType.ACCESSOR,
+        paths: expr.paths.slice(0)
+    }
+
+    prop = expr.paths[0].value
+    this.raw[prop] = immutableSet(this.raw[prop], expr.paths, 1, expr.paths.length, value, this)
+
+    this.fire({
+        type: DataChangeType.SET,
+        expr: expr,
+        value: value,
+        option: option
+    })
+
+    this.checkDataTypes()
 }
 
 export default Data
